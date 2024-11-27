e527bfb479f3d7b1fbb61a09fc7822c4490e907a
FCL00 <flagahit00@gmail.com>
2024-11-27 19:18:26 +0800

    add hero and search form

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,19 @@
 import Image from "next/image";
+import SearchForm from "@/components/SearchForm/SearchForm";
 
-export default function Home() {
+export default async function Home({searchParams} : {searchParams: Promise<{ query?: string}> }) {
+  const query = (await searchParams).query;
+
   return (
     <>
-      Home  
+        <section className="hero-container">
+          <h1 className="heading">Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt, vel.</h1>  
+          <p className="sub-heading">
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Molestias ad voluptate itaque, 
+            facilis nulla voluptatum voluptas dolorum sed impedit sit!
+          </p>
+          <SearchForm query={query} />
+        </section>
     </>
   );
 }
